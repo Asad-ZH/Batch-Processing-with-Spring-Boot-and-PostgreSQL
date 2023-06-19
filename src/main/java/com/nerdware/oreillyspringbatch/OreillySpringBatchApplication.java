7d6bea1be39fcb9aa723c19d84b7fce29b6e8f92
asad-jf <asadzahid1479@gmail.com>
2023-06-19 12:54:11 +0500

    Set Up Batch Processing Environment

---
+++ src/main/java/com/nerdware/oreillyspringbatch/OreillySpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.nerdware.oreillyspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OreillySpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OreillySpringBatchApplication.class, args);
+    }
+
+}
